4571e58b83c213fa2337a0a3363c287e6932dd39
simensan <simen.is@gmail.com>
2010-05-01 20:30:44 +0200

    You can now have a test that is only defined in lua, and the java @Before and @After are still run! I am unhappy with the amount of hacks and ugly overriding thats going on atm, and next item on the plan is to dig deeper into junit code to see what can be done.

---
+++ src/net/simensan/KahluaTest/LuaFrameworkMethod.java
@@ -0,0 +1,11 @@
+package net.simensan.KahluaTest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Simen
+ * Date: May 1, 2010
+ * Time: 2:02:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LuaFrameworkMethod {
+}
